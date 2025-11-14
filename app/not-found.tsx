457f422c106a4eddd6eaa3a49dda590c74ce6220
Santiago Ferreira <santiagoferreirati@gmail.com>
2025-11-14 18:54:54 -0300

    Forçando novo deploy

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,5 @@
 export const dynamic = "force-static"; // impede execução do Clerk no build
 
 export default function NotFound() {
-  return <h1>Página não encontrada</h1>;
+  return <h1>Página não encontradaa</h1>;
 }
